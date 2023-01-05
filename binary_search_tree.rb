0e4c7ba19433c38b2d4d5c0d8fb5c84b52721b56
Vickey Agarwal <vickeybansal842000@gmail.com>
2023-01-05 10:12:34 +0000

    made some changes in code

--- binary_search_tree.rb
+++ binary_search_tree.rb
@@ -1,115 +1,112 @@
 class Tree
-  attr_accessor :value, :left, :right
+	attr_accessor :value, :left, :right
 
-  def initialize(value) 
-    @value = value
-    @left = nil
-    @right = nil
-  end
+	def initialize(value) 
+		@value = value
+		@left = nil
+		@right = nil
+	end
 end
 
 class BST
-  attr_accessor :root
+	attr_accessor :root
 
-  def initialize
-    @root = nil
-  end
+	def initialize
+		@root = nil
+	end
 end
 
 def insert(value)
-  if self.root.nil
-  self.root = Tree.new(value) 
-  else 
-    current_node = self.root 
-    previous_node = self.root 
-    while current_node.nil 
-      previous_node = current_node 
-      if value < current_node.value
-        current_node = current_node.left
-      else 
-        current_node = current_node.right
-      end
-    end
-    if value < previous_node.value
-      previous_node.left = Tree.new(value)
-    else 
-      previous_node.right = Tree.new(value)
-    end
-  end
+	if self.root.nil?
+		self.root = Tree.new(value) 
+	else 
+		current_node = self.root 
+		previous_node = self.root 
+		while !current_node.nil? 
+			previous_node = current_node 
+			if value < current_node.value
+				current_node = current_node.left
+			else
+				current_node = current_node.right
+			end
+		end
+		if value < previous_node.value
+			previous_node.left = Tree.new(value)
+		else 
+			previous_node.right = Tree.new(value)
+		end
+	end
 end
 
 def largest_element(node=self.root)
-  return nil if node.nil
-  return node.value if node.right.nil
-  return largest_element(node.right)
+	return nil if node.nil?
+	return node.value if node.right.nil?
+	return largest_element(node.right)
 end
 
 def smallest_element(node=self.root)
-  return node.value if node.left.nil
-  smallest_element(root.left)
+	return nil if node.nil?
+	return node.value if node.left.nil?
+	smallest_element(root.left)
 end
 
 def in_order(node = self.root)
-  return if node.nil
-  in_order(node.left)
-  print"#{node.value}"
-  print(" ")
-  in_order(node.right)
+	return if node.nil?
+	in_order(node.left)
+	print"#{node.value}"
+	print(" ")
+	in_order(node.right)
 end
 
 def pre_order(node=self.root)
-  return if node.nil
-  print"#{node.value}"
+	return if node.nil?
+	print"#{node.value}"
   print(" ")
-  pre_order(node.left)
-  pre_order(node.right)
+	pre_order(node.left)
+	pre_order(node.right)
 end
 
 def post_order(node=self.root)
-  return if root.nil
-  post_order(node.left)
-  post_order(node.right)
-  print"#{node.value}"
-  print(" ")
+	return if root.nil?
+	post_order(node.left)
+	post_order(node.right)
+	print"#{node.value}"
+	print(" ")
 end
 
 def in_order(node=self.root)
-  return if node.nil
-  in_order(node.left)
-  print"#{node.value}"
-  print(" ")
-  in_order(node.right)
-  end
+	return if node.nil?
+	in_order(node.left)
+	print"#{node.value}"
+	print(" ")
+	in_order(node.right)
 end  
 
 def search_element(node = self.root,element)
-  return false if node.nil
-  return true if element == node.value
-  elsif element < node.value
+	return false if node.nil?
+	return true if element == node.value
+	if element < node.value
+	end
 end
-
-#review again
 def remove_element(value,node = self.root)
-  return nil if node.nil
-  if value < node.value
-    node.left = remove_element(value, node.left)
-  elsif node.value < value
-    node.right = remove_element(value, node.right)
-  else
-      if node.left.nil && node.right.nil
-        temp = node
-        min_of_right_subtree = find_min(node.right)
-        node.value = min_of_right_subtree.value
-        node.right = remove_element(min_of_right_subtree.value, node.right)
-      elsif node.left.nil
-        node = node.left
-      elsif node.right.nil
-        node = node.right
-      else
-        node = nil
-      end
-    end
-  node
+	return nil if node.nil?
+	if value < node.value
+		node.left = remove_element(value, node.left)
+	elsif node.value < value
+		node.right = remove_element(value, node.right)
+	elsif node.left.nil? && node.right.nil?
+		temp = node
+		min_of_right_subtree = find_min(node.right)
+		node.value = min_of_right_subtree.value
+		node.right = remove_element(min_of_right_subtree.value, node.right)
+	elsif node.left.nil?
+		node = node.left
+	elsif node.right.nil?
+		node = node.right
+	else
+		node = nil
+	end
+	node
 end
 
 puts "Enter number which operation you want to perform: "
@@ -129,61 +126,65 @@ root = BST.new()
 loop_end = false
 fileobject = File.new("element_file.txt","a+")
 def elements_file(node = self.root)
-  return if node.nill
-  elements_file(node.left)
-  fileobject.syswrite(node.value)
-  elements_file(node.right)
+	return if node.nill
+	elements_file(node.left)
+	fileobject.syswrite(node.value)
+	elements_file(node.right)
+end
+
 def load_BST
-  root = BST.new() 
-  elements = fileobject.read()
-  for i in 1..elements.length
-    insert(value)
-  end
+	root = BST.new() 
+	elements = fileobject.read()
+	for i in 1..elements.length
+		insert(value)
+	end
+end
 
 while(!loop_end)
-  operation_num = gets.chomp()
-  operation_num.to_i
-  case operation_num
-  when 1
-    elements = []
-    print("How much values you want to add in BST:")
-    num_values = gets.chomp()
-    for i in 1..num_values
-      value = gets()
-      print(", ")
-      insert(value)
-    end
-  when 2
-    largest_element(root)
-  when 3
-    smallest_element(root)
-  when 4
-    in_order(root)
-  when 5
-    post_order(root)
-  when 6
-    level_order(root)
-  when 7
-    pre_order(root)
-  when 8
-    print("Enter element which you want to search:")
-    element = gets.chomp()
-    search_element(root,element)
-  when 9
-    print("Enter element which you want to remove:")
-    element = gets.chomp()
-    print(remove_element(root,element))
-  when 10
-    print(all_paths(root))
+	operation_num = gets.chomp()
+	operation_num.to_i
+	case operation_num
+	when 1
+		elements = []
+		print("How much values you want to add in BST:")
+		num_values = gets.chomp()
+		for i in 1..num_values
+			value = gets()
+			print(", ")
+			insert(value)
+		end
+	when 2
+		largest_element(root)
+	when 3
+		smallest_element(root)
+	when 4
+		in_order(root)
+	when 5
+		post_order(root)
+	when 6
+		level_order(root)
+	when 7
+		pre_order(root)
+	when 8
+		print("Enter element which you want to search:")
+		element = gets.chomp()
+		search_element(root,element)
+	when 9
+		print("Enter element which you want to remove:")
+		element = gets.chomp()
+		print(remove_element(root,element))
+	when 10
+		print(all_paths(root))
   when 11
-    name = gets()
-    if name = element_file
-      load_bst()
-  when 12
-    elements_file(root)
-    print("exit")
+		name = gets()
+		if name = element_file
+			load_bst()
+		end
+	when 12
+		elements_file(root)
+		print("exit")
     loop_end = true
-  else 
-    "Default value"
-  end
+	else 
+		"Default value"
+	end
 end
