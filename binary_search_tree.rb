ad5d9e0605ebbc7afeba03b824be49b188d2fb67
vickey8 <vickeybansal842000@gmaail.com>
2023-01-06 07:42:58 +0000

    review at github

--- binary_search_tree.rb
+++ binary_search_tree.rb
@@ -1,22 +1,22 @@
-class Tree
-  attr_accessor :value, :left, :right
+class TreeNode
+	attr_accessor :value, :left, :right
 
-  def initialize(value) 
-    @value = value
-    @left = nil
-    @right = nil
-  end
+	def initialize(value) 
+		@value = value
+		@left = nil
+		@right = nil
+	end
 end
 
 class BST
 	attr_accessor :root
 
-  def initialize
+	def initialize
 		@root = nil
 	end
 	def insert(value)
 		if self.root.nil?
-			self.root = Tree.new(value) 
+			self.root = TreeNode.new(value) 
 		else 
 			current_node = self.root 
 			previous_node = self.root 
@@ -25,9 +25,9 @@ class BST
 				value < current_node.value ? current_node.left : current_node = current_node.right
 			end
 			if value < previous_node.value
-				previous_node.left = Tree.new(value)
+				previous_node.left = TreeNode.new(value)
 			else 
-				previous_node.right = Tree.new(value)
+				previous_node.right = TreeNode.new(value)
 			end
 		end
 	end
@@ -35,7 +35,7 @@ class BST
 	def largest_element(node = self.root)
 		return node if node.nil?
 		return node.value if node.right.nil?
-		return largest_element(node.right)
+		largest_element(node.right)
 	end
 
 	def smallest_element(node = self.root)
@@ -47,15 +47,13 @@ class BST
 	def in_order(node = self.root)
 		return if node.nil?
 		in_order(node.left)
-		print "#{node.value}"
-		print(" ")
+		print "#{node.value} "
 		in_order(node.right)
 	end
 
 	def pre_order(node = self.root)
 		return if node.nil?
-		print "#{node.value}"
-		print(" ")
+		print "#{node.value} "
 		pre_order(node.left)
 		pre_order(node.right)
 	end
@@ -64,25 +62,22 @@ class BST
 		return if root.nil?
 		post_order(node.left)
 		post_order(node.right)
-		print "#{node.value}"
-		print(" ")
+		print "#{node.value} "
 	end  
 
-	def search_element(node = self.root,element)
+	def search_element(node = self.root, element)
 		return false if node.nil?
 		return search_element(node.left, element) if element < node.value
 		return search_element(node.right, element) if node.value < element
 		true
 	end
 
-	def all_path(node = self.root, arr)
+	def all_path(node = self.root, elements_array)
 		return if node.nil?
-		arr.push(node.value)
-		if(node.left.nil? && node.right.nil?)
-			print " " , arr
-		end
-		all_path(node.left, arr)
-		all_path(node.right, arr)
+		elements_array.push(node.value)
+		print " #{elements_array}" if node.left.nil? && node.right.nil?
+		all_path(node.left, elements_array)
+		all_path(node.right, elements_array)
 		arr.pop()
 	end
 
@@ -93,7 +88,7 @@ class BST
 		elsif node.value < value
 			node.right = remove_element(value, node.right)
 		else
-			if(node.left.nil? && node.right.nil?)
+			if node.left.nil? && node.right.nil?
 				temp = node
 				min_of_right_subtree = find_min(node.right)
 				node.value = min_of_right_subtree.value
@@ -108,83 +103,78 @@ class BST
 		end
 		node
 	end
+end
+fileobject = File.new("elements_file.txt", "a+")
+def add_elements_to_file(node = self.root)
+	return if node.nil?
+	add_elements_to_file(node.left)
+	fileobject.syswrite(node.value)
+	add_elements_to_file(node.right)
+end
 
-	def elements_file(node = self.root)
-		return if node.nil?
-		elements_file(node.left)
-		fileobject.syswrite(node.value)
-		elements_file(node.right)
-	end
-
-	def load_BST
-		root = BST.new
-		elements = fileobject.read()
-		for i in 1..elements.length
-			insert(value)
-		end
-	end
-
-	puts "Enter number which operation you want to perform: "
-	puts "1.Add elements in BST"
-	puts "2.Print largest element"
-	puts "3.Print smallest element"
-	puts "4.Print inorder of BST"
-	puts "5.Print postorder of BST"
-	puts "6.Print levelorder of BST"
-	puts "7.Print preorder of BST"
-	puts "8.Check whether element is present or not"
-	puts "9.Remove an element from BST"
-	puts "10.Print all the paths i.e starting from the root to the leaf"
-	puts "11.Enter the file name for previous values in BST"
-	puts "12.Quit"
-
+def load_BST
 	root = BST.new
-	loop_end = false
-	while(!loop_end)
-		operation_num = gets.chomp()
-		case operation_num.to_i
-		when 1
-			print("How much values you want to add in BST:")
-			num_values = gets.chomp
-			for i in 1..num_values.to_i
-				value = gets
-				print(", ")
-				insert(value.to_i)
-			end
-		when 2
-			print(largest_element)
-		when 3
-			print(smallest_element)
-		when 4
-			in_order(root)
-		when 5
-			post_order(root)
-		when 6
-			level_order(root)
-		when 7
-			pre_order(root)
-		when 8
-			print("Enter element which you want to search:")
-			element = gets.chomp()
-			search_element(root, element.to_i)
-		when 9
-			print("Enter element which you want to remove:")
-			element = gets.chomp
-			print(remove_element(element.to_i))
-		when 10
-			arr = Array.new
-			all_paths(root, arr)
-		when 11
-			name = gets()
-			if name == element_file
-				load_bst
-			end
-		when 12
-			elements_file(root)
-			print("exit")
-			loop_end = true
-		else 
-			puts "Default value"
+	elements = fileobject.read()
+	elements.each { |element| insert(element) }
+end
+puts "Enter number which operation you want to perform: "
+puts "1.Add elements in BST"
+puts "2.Print largest element"
+puts "3.Print smallest element"
+puts "4.Print inorder of BST"
+puts "5.Print postorder of BST"
+puts "6.Print levelorder of BST"
+puts "7.Print preorder of BST"
+puts "8.Check whether element is present or not"
+puts "9.Remove an element from BST"
+puts "10.Print all the paths i.e starting from the root to the leaf"
+puts "11.Enter the file name for previous values in BST"
+puts "12.Quit"
+
+root = BST.new
+while(true)
+	operation_num = gets.chomp
+	case operation_num.to_i
+	when 1
+		puts "Enter values which you want to add in BST separated by comma:"
+		element_values = gets
+		element_values_array = element_values.split(",")
+		for element in element_values_array do
+			insert(element.to_i)
 		end
+	when 2
+		root ? print(root.largest_element) : print("Currently BST is empty")
+	when 3
+		root ? print(root.smallest_element) : print("Currently BST is empty")
+	when 4
+		root.in_order
+	when 5
+		root.post_order
+	when 6
+		root.level_order
+	when 7
+		root.pre_order
+	when 8
+		print("Enter element which you want to search:")
+		element = gets.chomp
+		search_element(root, element.to_i)
+	when 9
+		print("Enter element which you want to remove:")
+		element = gets.chomp
+		print(remove_element(element.to_i))
+	when 10
+		elements_array = Array.new
+		root.all_paths(root, elements_array)
+	when 11
+		name = gets
+		if name == elements_file && elements_file?
+			load_BST
+		end
+	when 12
+		add_elements_to_file
+		print("exit")
+		break
+	else
+		puts "Please enter value from given options"
 	end
 end
